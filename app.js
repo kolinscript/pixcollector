059f9a9cfd4b9b5f3c329103eb656c7b440b73c8
nkoshkarov <nkoshkarov@phoenixit.ru>
2020-03-24 04:08:59 +0300

    vk oauth token

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express            = require('express');
 const path               = require('path');
 const bodyParser         = require('body-parser');
+const request            = require('request');
 const port               = process.env.PORT || 5000;
 const app                = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.post('/auth', (req, res, next) => {
+    const { body: { link } } = req;
+    request(link, function (error, response, body) {
+        res.send(body);
+    });
+});
 
 app.get('/photos', (req, res, next) => {
 });
--- client/src/App.js
+++ client/src/App.js
@@ -37,10 +37,15 @@ class App extends Component {
                 `&client_secret=XgglLIZcB7qB3nNryc8y` +
                 `&redirect_uri=https://pixcollector.herokuapp.com` +
                 `&code=${code}`;
-            // window.open(AUTH_URL_TOKEN,"_self");
-            fetch(AUTH_URL_TOKEN, {
-                mode: 'cors',
-                credentials: 'include'
+            const data = {
+                link: AUTH_URL_TOKEN
+            };
+            fetch('/auth', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json', 'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
             })
                 .then(res => res.json())
                 .then((data) => {console.log(data)})
