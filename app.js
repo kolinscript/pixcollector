3d3aa670854728a7a700d1a494f6c1fff7e83618
nkoshkarov <nkoshkarov@phoenixit.ru>
2020-03-24 06:27:07 +0300

    get vk photo albums debug

--- app.js
+++ app.js
@@ -9,23 +9,21 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const data = {
-    access_token: '',
-    user_id: ''
-};
+var access_token;
+var user_id;
 const vkApiVersion = '5.103';
 
 app.post('/auth', (req, res, next) => {
     const { body: { link } } = req;
     request(link, function (error, response, body) {
-        data.access_token = body.access_token;
-        data.user_id = body.user_id;
-        res.send(body.access_token);
+        access_token = body.access_token;
+        user_id = body.user_id;
+        res.send(body, response);
     });
 });
 
 app.get('/photos', (req, res, next) => {
-    const link = `https://api.vk.com/method/photos.getAlbums?owner_id=${data.user_id}&access_token=${data.access_token}&v=${vkApiVersion}`;
+    const link = `https://api.vk.com/method/photos.getAlbums?owner_id=${user_id}&access_token=${access_token}&v=${vkApiVersion}`;
     request(link, function (error, response, body) {
         res.send({
             body: body,
