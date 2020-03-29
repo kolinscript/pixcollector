c5195006ece03d51be25d7b2df4f7578b4287c70
nkoshkarov <nkoshkarov@phoenixit.ru>
2020-03-29 20:31:35 +0300

    app structure optimization

--- api/app.js
+++ api/app.js
@@ -6,14 +6,17 @@ const ZipStream          = require('zip-stream');
 const port               = process.env.PORT || 5000;
 const app                = express();
 const state              = require('state');
+const apiV1              = require('./routes/v1');
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use('/api/v1', apiV1);
 app.set('state', state);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'../client/build/index.html'));
 });
+
 
 app.listen(port, () => console.log('Api live on port', + port));
