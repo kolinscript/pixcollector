c8fc25e2ab15f82c1cd075b4d48d21109b671de3
nkoshkarov <nkoshkarov@phoenixit.ru>
2020-03-30 03:23:19 +0300

    api auth optimization test

--- api/routes/v1/auth.js
+++ api/routes/v1/auth.js
@@ -2,9 +2,8 @@ const router             = require('express').Router();
 const request            = require('request');
 
 router.get('/', (req, res, next) => {
-    console.log('REQ', req);
     const state = req.app.get('state').state;
-    const { code: { code } } = req.query.code;
+    const code = req.query.code;
     console.log('CODE', code);
     const link = `https://oauth.vk.com/access_token` +
         `?client_id=7372433` +
