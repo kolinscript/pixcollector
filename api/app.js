61437758787daa5fb8fd47bcadccfad58244e0f3
nkoshkarov <nkoshkarov@phoenixit.ru>
2020-03-30 17:37:43 +0300

    api react router test

--- api/app.js
+++ api/app.js
@@ -6,11 +6,6 @@ const apiV1              = require('./routes/v1');
 const port               = process.env.PORT || 5000;
 const app                = express();
 
-// old
-// app.use(express.static(path.join(__dirname, '../client/build')));
-// new
-// app.use('/static', express.static(path.join(__dirname, '../client/build//static')));
-// the newest
 app.use('/', express.static(path.join(__dirname, '../client/build')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,15 +13,6 @@ app.use(bodyParser.json());
 app.use('/api/v1', apiV1);
 app.set('state', state);
 
-// old
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname+'../client/build/index.html'));
-// });
-// new
-// app.get('*', (req, res) => {
-//     res.sendFile('index.html', {root: path.join(__dirname, '../client/build/')});
-// });
-// the newest
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
