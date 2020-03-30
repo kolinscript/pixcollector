31f1b2048e7e4022295cafe9d7e079a636fcba45
nkoshkarov <nkoshkarov@phoenixit.ru>
2020-03-30 17:32:57 +0300

    api react router test

--- api/app.js
+++ api/app.js
@@ -6,18 +6,29 @@ const apiV1              = require('./routes/v1');
 const port               = process.env.PORT || 5000;
 const app                = express();
 
+// old
 // app.use(express.static(path.join(__dirname, '../client/build')));
-app.use('/static', express.static(path.join(__dirname, '../client/build//static')));
+// new
+// app.use('/static', express.static(path.join(__dirname, '../client/build//static')));
+// the newest
+app.use('/', express.static(path.join(__dirname, '../client/build')));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/api/v1', apiV1);
 app.set('state', state);
 
+// old
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname+'../client/build/index.html'));
 // });
-app.get('*', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, '../client/build/')});
+// new
+// app.get('*', (req, res) => {
+//     res.sendFile('index.html', {root: path.join(__dirname, '../client/build/')});
+// });
+// the newest
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(port, () => console.log('Api live on port', + port));
