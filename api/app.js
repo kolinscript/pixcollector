89a77e91f205ab580523d536705248e3e135da2e
nkoshkarov <nkoshkarov@phoenixit.ru>
2020-04-01 02:48:03 +0300

    add mongo db

--- api/app.js
+++ api/app.js
@@ -1,12 +1,33 @@
 const express            = require('express');
 const path               = require('path');
 const bodyParser         = require('body-parser');
+const cors               = require('cors');
 const session            = require('express-session');
+const mongoose           = require('mongoose');
 const MongoStore         = require('connect-mongo')(session);
 const apiV1              = require('./routes/v1');
 const port               = process.env.PORT || 5000;
+const db                 = process.env.MONGO;
 const app                = express();
 
+const connectDB          = async () => {
+    try {
+        await mongoose.connect(db, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+        console.log("MongoDB is Connected...");
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+app.use(cors());
+app.use(session({
+    secret: "SOME_SECRET_KEY",
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
 app.use('/', express.static(path.join(__dirname, '../client/build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
