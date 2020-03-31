8985cc2929a534c2687317b653adef8900a7eb69
nkoshkarov <nkoshkarov@phoenixit.ru>
2020-04-01 02:58:35 +0300

    add mongo db

--- api/app.js
+++ api/app.js
@@ -10,18 +10,12 @@ const port               = process.env.PORT || 5000;
 const db                 = process.env.MONGO;
 const app                = express();
 
-const connectDB          = async () => {
-    try {
-        await mongoose.connect(db, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
-        console.log("MongoDB is Connected...");
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-};
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(
+        () => { console.log('Database is connected'); },
+        (err) => { console.log('Can not connect to the database' + err); }
+    );
 
 app.use(cors());
 app.use(session({
