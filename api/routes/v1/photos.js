78373b78c57ab33e438b75a208f2bdd17b8d6b41
nkoshkarov <koshkarovnik@gmail.com>
2020-04-02 22:32:48 +0300

    photos endpoint reworked to axios

--- api/routes/v1/photos.js
+++ api/routes/v1/photos.js
@@ -1,4 +1,6 @@
-const router = require('express').Router();
+const router             = require('express').Router();
+const axios              = require('axios');
+
 const request = require('request');
 
 router.get('/', (req, res, next) => {
@@ -39,46 +41,54 @@ router.get('/', (req, res, next) => {
             `&offset=${reqOffset}` +
             `&count=${reqFloatPart}` +
             `&v=5.103`;
-        request(link, function (error, response, body) {
-            const bodyParsed = JSON.parse(body);
-            const arr = [];
-            if (bodyParsed.response) {
-                bodyParsed.response.items.forEach((item) => {
-                    // ascending flow
-                    // S -> M -> X -> Y -> Z -> W
-                    const sizeW = item.sizes.find(size => size.type === 'w');
-                    const sizeZ = item.sizes.find(size => size.type === 'z');
-                    const sizeY = item.sizes.find(size => size.type === 'y');
-                    const sizeX = item.sizes.find(size => size.type === 'x');
-                    const sizeM = item.sizes.find(size => size.type === 'm');
-                    const sizeS = item.sizes.find(size => size.type === 's');
-                    if (sizeW) {
-                        arr.push(sizeW);
-                    } else if (sizeZ) {
-                        arr.push(sizeZ);
-                    } else if (sizeY) {
-                        arr.push(sizeY);
-                    } else if (sizeX) {
-                        arr.push(sizeX);
-                    } else if (sizeM) {
-                        arr.push(sizeM);
-                    } else if (sizeS) {
-                        arr.push(sizeS);
-                    }
-                    pixArray = arr;
-                    req.session.pixArray = pixArray;
+        axios.get(link)
+            .then(function (response) {
+                console.log(response);
+                const arr = [];
+                if (response.response) {
+                    response.response.items.forEach((item) => {
+                        // ascending flow
+                        // S -> M -> X -> Y -> Z -> W
+                        const sizeW = item.sizes.find(size => size.type === 'w');
+                        const sizeZ = item.sizes.find(size => size.type === 'z');
+                        const sizeY = item.sizes.find(size => size.type === 'y');
+                        const sizeX = item.sizes.find(size => size.type === 'x');
+                        const sizeM = item.sizes.find(size => size.type === 'm');
+                        const sizeS = item.sizes.find(size => size.type === 's');
+                        if (sizeW) {
+                            arr.push(sizeW);
+                        } else if (sizeZ) {
+                            arr.push(sizeZ);
+                        } else if (sizeY) {
+                            arr.push(sizeY);
+                        } else if (sizeX) {
+                            arr.push(sizeX);
+                        } else if (sizeM) {
+                            arr.push(sizeM);
+                        } else if (sizeS) {
+                            arr.push(sizeS);
+                        }
+                        pixArray = arr;
+                        req.session.pixArray = pixArray;
+                    });
+                }
+                res.send({
+                    body: pixArray
                 });
-            }
-            res.send({
-                body: pixArray
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
             });
-        });
     }
     else if (integerPart > 0) {
         // multiple requests
         // handling integerPart
-        let offset = reqOffset;
-        for (let offset = offset, count = 1000; count <= reqIntegerPart; offset = offset + 1000, count = count + 1000) {
+        let offsetLast;
+        let urlArray = [];
+        for (let offset = reqOffset, count = 1000; count <= reqIntegerPart; offset = offset + 1000, count = count + 1000) {
             const link = `https://api.vk.com/` +
                 `method/photos.get` +
                 `?owner_id=${req.session.user_id}` +
@@ -88,89 +98,56 @@ router.get('/', (req, res, next) => {
                 `&offset=${offset}` +
                 `&count=${count}` +
                 `&v=5.103`;
-            photosFetcher = async function () {
-                const promises = request(link, function (error, response, body) {
-                    const bodyParsed = JSON.parse(body);
-                    const arr = [];
-                    if (bodyParsed.response) {
-                        bodyParsed.response.items.forEach((item) => {
-                            // ascending flow
-                            // S -> M -> X -> Y -> Z -> W
-                            const sizeW = item.sizes.find(size => size.type === 'w');
-                            const sizeZ = item.sizes.find(size => size.type === 'z');
-                            const sizeY = item.sizes.find(size => size.type === 'y');
-                            const sizeX = item.sizes.find(size => size.type === 'x');
-                            const sizeM = item.sizes.find(size => size.type === 'm');
-                            const sizeS = item.sizes.find(size => size.type === 's');
-                            if (sizeW) {
-                                arr.push(sizeW);
-                            } else if (sizeZ) {
-                                arr.push(sizeZ);
-                            } else if (sizeY) {
-                                arr.push(sizeY);
-                            } else if (sizeX) {
-                                arr.push(sizeX);
-                            } else if (sizeM) {
-                                arr.push(sizeM);
-                            } else if (sizeS) {
-                                arr.push(sizeS);
-                            }
-                            pixArray = pixArray.concat(arr);
-                        });
-                    }
-                });
-
-                await Promise.all(promises);
-                console.log(`All async tasks complete!`)
-            };
-            photosFetcher();
+            urlArray.push(link);
+            offsetLast = offset + 1000;
         }
-        console.log(offset);
-        const link = `https://api.vk.com/` +
+        const linkLast = `https://api.vk.com/` +
             `method/photos.get` +
             `?owner_id=${req.session.user_id}` +
             `&access_token=${req.session.access_token}` +
             `&album_id=saved` +
             `&photo_sizes=1` +
-            `&offset=${offset}` +
+            `&offset=${offsetLast}` +
             `&count=${reqFloatPart}` +
             `&v=5.103`;
-        request(link, function (error, response, body) {
-            const bodyParsed = JSON.parse(body);
-            const arr = [];
-            if (bodyParsed.response) {
-                bodyParsed.response.items.forEach((item) => {
-                    // ascending flow
-                    // S -> M -> X -> Y -> Z -> W
-                    const sizeW = item.sizes.find(size => size.type === 'w');
-                    const sizeZ = item.sizes.find(size => size.type === 'z');
-                    const sizeY = item.sizes.find(size => size.type === 'y');
-                    const sizeX = item.sizes.find(size => size.type === 'x');
-                    const sizeM = item.sizes.find(size => size.type === 'm');
-                    const sizeS = item.sizes.find(size => size.type === 's');
-                    if (sizeW) {
-                        arr.push(sizeW);
-                    } else if (sizeZ) {
-                        arr.push(sizeZ);
-                    } else if (sizeY) {
-                        arr.push(sizeY);
-                    } else if (sizeX) {
-                        arr.push(sizeX);
-                    } else if (sizeM) {
-                        arr.push(sizeM);
-                    } else if (sizeS) {
-                        arr.push(sizeS);
-                    }
-                    pixArray = pixArray.concat(arr);
-                });
-            }
-        });
+        urlArray.push(linkLast);
 
-        req.session.pixArray = pixArray;
-        console.log(pixArray);
-        res.send({
-            body: pixArray
-        });
+        axios.all(urlArray)
+            .then(function(responses) {
+                let temp = responses.map(r => r.response);
+                console.log(response);
+                const arr = [];
+                if (temp) {
+                    temp.items.forEach((item) => {
+                        // ascending flow
+                        // S -> M -> X -> Y -> Z -> W
+                        const sizeW = item.sizes.find(size => size.type === 'w');
+                        const sizeZ = item.sizes.find(size => size.type === 'z');
+                        const sizeY = item.sizes.find(size => size.type === 'y');
+                        const sizeX = item.sizes.find(size => size.type === 'x');
+                        const sizeM = item.sizes.find(size => size.type === 'm');
+                        const sizeS = item.sizes.find(size => size.type === 's');
+                        if (sizeW) {
+                            arr.push(sizeW);
+                        } else if (sizeZ) {
+                            arr.push(sizeZ);
+                        } else if (sizeY) {
+                            arr.push(sizeY);
+                        } else if (sizeX) {
+                            arr.push(sizeX);
+                        } else if (sizeM) {
+                            arr.push(sizeM);
+                        } else if (sizeS) {
+                            arr.push(sizeS);
+                        }
+                        pixArray = arr;
+                        req.session.pixArray = pixArray;
+                    });
+                }
+                res.send({
+                    body: pixArray
+                });
+            });
     }
 });
 
