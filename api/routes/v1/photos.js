1fe7da33434d870b4a2618c3ad37813b74829d49
nkoshkarov <koshkarovnik@gmail.com>
2020-04-02 17:08:40 +0300

    update photos.js endpoint test

--- api/routes/v1/photos.js
+++ api/routes/v1/photos.js
@@ -69,14 +69,57 @@ router.get('/', (req, res, next) => {
                     req.session.pixArray = pixArray;
                 });
             }
-        });
-        res.send({
-            body: pixArray
+            res.send({
+                body: pixArray
+            });
         });
     } else if (integerPart > 0) {
         // multiple requests
         // handling integerPart
-        for (let offset = reqOffset, count = 1000; count <= reqIntegerPart; offset = offset + 1000, count = count + 1000) {
+        const photoFetcher = new Promise((resolve, reject) => {
+            let offset = reqOffset;
+            for (offset, count = 1000; count <= reqIntegerPart; offset = offset + 1000, count = count + 1000) {
+                const link = `https://api.vk.com/` +
+                    `method/photos.get` +
+                    `?owner_id=${req.session.user_id}` +
+                    `&access_token=${req.session.access_token}` +
+                    `&album_id=saved` +
+                    `&photo_sizes=1` +
+                    `&offset=${offset}` +
+                    `&count=${count}` +
+                    `&v=5.103`;
+                request(link, function (error, response, body) {
+                    const bodyParsed = JSON.parse(body);
+                    const arr = [];
+                    if (bodyParsed.response) {
+                        bodyParsed.response.items.forEach((item) => {
+                            // ascending flow
+                            // S -> M -> X -> Y -> Z -> W
+                            const sizeW = item.sizes.find(size => size.type === 'w');
+                            const sizeZ = item.sizes.find(size => size.type === 'z');
+                            const sizeY = item.sizes.find(size => size.type === 'y');
+                            const sizeX = item.sizes.find(size => size.type === 'x');
+                            const sizeM = item.sizes.find(size => size.type === 'm');
+                            const sizeS = item.sizes.find(size => size.type === 's');
+                            if (sizeW) {
+                                arr.push(sizeW);
+                            } else if (sizeZ) {
+                                arr.push(sizeZ);
+                            } else if (sizeY) {
+                                arr.push(sizeY);
+                            } else if (sizeX) {
+                                arr.push(sizeX);
+                            } else if (sizeM) {
+                                arr.push(sizeM);
+                            } else if (sizeS) {
+                                arr.push(sizeS);
+                            }
+                            pixArray = pixArray.concat(arr);
+                            req.session.pixArray = pixArray;
+                        });
+                    }
+                });
+            }
             const link = `https://api.vk.com/` +
                 `method/photos.get` +
                 `?owner_id=${req.session.user_id}` +
@@ -84,7 +127,7 @@ router.get('/', (req, res, next) => {
                 `&album_id=saved` +
                 `&photo_sizes=1` +
                 `&offset=${offset}` +
-                `&count=${count}` +
+                `&count=${reqFloatPart}` +
                 `&v=5.103`;
             request(link, function (error, response, body) {
                 const bodyParsed = JSON.parse(body);
@@ -117,10 +160,14 @@ router.get('/', (req, res, next) => {
                     });
                 }
             });
-        }
-        console.log(pixArray);
-        res.send({
-            body: pixArray
+            resolve();
+        });
+
+        photoFetcher.then(() => {
+            console.log(pixArray);
+            res.send({
+                body: pixArray
+            });
         });
     }
 });
