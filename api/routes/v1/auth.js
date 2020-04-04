976d4ffa8bfba88980ca58dbbc014830b2c75785
nkoshkarov <koshkarovnik@gmail.com>
2020-04-04 17:45:11 +0300

    mongo express-session fixed

--- api/routes/v1/auth.js
+++ api/routes/v1/auth.js
@@ -12,26 +12,25 @@ router.get('/', secure.optional, (req, res, next) => {
         `&redirect_uri=https://pixcollector.herokuapp.com/api/v1/auth` +
         `&code=${code}`;
     axios.get(authLink)
-        .then(function (response) {
+        .then(function (response1) {
             const albumLink = `https://api.vk.com/` +
                 `method/photos.getAlbums` +
-                `?owner_id=${response.data.user_id}` +
-                `&access_token=${response.data.access_token}` +
+                `?owner_id=${response1.data.user_id}` +
+                `&access_token=${response1.data.access_token}` +
                 `&need_system=1` +
                 `&v=5.103`;
             axios.get(albumLink)
-                .then(function (response) {
-                    const albumSize = response.data.response.items.find(item => item.id === -15).size;
+                .then(function (response2) {
+                    const albumSize = response2.data.response.items.find(item => item.id === -15).size;
                     const user = new Users({
-                        vkId: response.data.user_id,
-                        vkToken: response.data.access_token,
+                        vkId: response1.data.user_id,
+                        vkToken: response1.data.access_token,
                         albumSize: albumSize
                     });
                     // save user to db
                     user.save()
                         .then(() => {
                                 req.session.user = user.toAuthJSON();
-                                console.log('req.session.user: ', req.session.user);
                                 res.redirect('/stock');
                             }
                         );
--- api/routes/v1/photos.js
+++ api/routes/v1/photos.js
@@ -163,8 +163,6 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/albumSize', (req, res, next) => {
-    console.log('req.session', req.session);
-    console.log('req.session.user', req.session.user);
     res.send({
         album_size: req.session.user.albumSize
     });
