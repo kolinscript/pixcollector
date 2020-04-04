7769b2f75a97f7b3833df69e08fa93a7a22b17bc
nkoshkarov <koshkarovnik@gmail.com>
2020-04-04 22:52:26 +0300

    user.js endpoint test

--- api/routes/v1/user.js
+++ api/routes/v1/user.js
@@ -4,8 +4,9 @@ const mongoose           = require('mongoose');
 const Users              = mongoose.model('Users');
 
 router.get('/', secure.optional, (req, res, next) => {
+    console.log('req.session.user.vkId', req.session.user.vkId);
     Users.find({
-        "vkId": req.session.user.vkId
+        vkId: req.session.user.vkId
     }, (err, user) => {
         if (err) return console.error(err);
         return res.status(200).json( { body: { user: user } });
