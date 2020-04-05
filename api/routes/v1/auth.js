060d3df1b64026029e1c75cf07881929cdb32cbe
nkoshkarov <koshkarovnik@gmail.com>
2020-04-05 20:17:50 +0300

    token test succeed test unauthorized error

--- api/routes/v1/auth.js
+++ api/routes/v1/auth.js
@@ -63,6 +63,7 @@ router.get('/', secure.optional, (req, res, next) => {
 });
 
 router.get('/success', secure.optional, (req, res, next) => {
+    console.log(req.session.user);
     if (req.session.user) {
         res.status(200).json( { body: { token: req.session.user.token } });
     } else {
