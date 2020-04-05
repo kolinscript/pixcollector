31213b4b127a0fcce46d622267d098c24ef449f1
nkoshkarov <koshkarovnik@gmail.com>
2020-04-06 01:58:35 +0300

    interceptor debug

--- client/src/interceptor.js
+++ client/src/interceptor.js
@@ -1,4 +1,5 @@
 import fetchInterceptor from 'fetch-interceptor';
+import fetchIntercept from 'fetch-intercept';
 
 const getToken = () => {
     const token = localStorage.getItem('token');
@@ -6,22 +7,49 @@ const getToken = () => {
 };
 
 // Register interceptor hooks
-export const interceptor = fetchInterceptor.register({
-    onBeforeRequest(request, controller) {
-        console.log('** interceptor ** request: ', request);
-        console.log('** interceptor ** controller: ', controller);
+// export const interceptor = fetchInterceptor.register({
+//     onBeforeRequest(request, controller) {
+//         console.log('** interceptor ** request: ', request);
+//         console.log('** interceptor ** controller: ', controller);
+//         if (getToken()) {
+//             request.headers.Authorization = `x-csrf-token ${getToken()}`;
+//         }
+//     },
+//     onRequestSuccess(response, request) {
+//         // Hook on response success
+//     },
+//     onRequestFailure(response, request) {
+//         console.log('** interceptor ** error response: ', response);
+//         if (response.status === 401) {
+//             // localStorage.clear();
+//             // window.location = 'https://pixcollector.herokuapp.com/auth'; // redirect to AUTH
+//         }
+//     }
+// });
+
+export const interceptor = fetchIntercept.register({
+    request: function (url, config) {
+        // Modify the url or config here
         if (getToken()) {
-            request.headers.Authorization = `x-csrf-token ${getToken()}`;
+            config.headers.Authorization = `x-csrf-token ${getToken()}`;
         }
+        return [url, config];
     },
-    onRequestSuccess(response, request) {
-        // Hook on response success
+
+    requestError: function (error) {
+        // Called when an error occured during another 'request' interceptor call
+        console.log('** interceptor ** requestError: ', error);
+        return Promise.reject(error);
     },
-    onRequestFailure(response, request) {
-        console.log('** interceptor ** error response: ', response);
-        if (response.status === 401) {
-            // localStorage.clear();
-            // window.location = 'https://pixcollector.herokuapp.com/auth'; // redirect to AUTH
-        }
+
+    response: function (response) {
+        // Modify the reponse object
+        return response;
+    },
+
+    responseError: function (error) {
+        // Handle an fetch error
+        console.log('** interceptor ** responseError: ', error);
+        return Promise.reject(error);
     }
 });
