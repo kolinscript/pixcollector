f86e1d3574c1cc502acfe6c8957c7eda296906dc
nkoshkarov <koshkarovnik@gmail.com>
2020-04-05 17:37:21 +0300

    token test succeed test unauthorized error

--- client/src/interceptor.js
+++ client/src/interceptor.js
@@ -5,36 +5,39 @@ const getToken = () => {
     return token;
 };
 
-export const unregister = fetchIntercept.register({
-    request: function (url, config) {
-        if (getToken()) {
-            config.headers.Authorization = `x-csrf-token ${getToken()}`;
+export const unregister = {
+    errorHandler: fetchIntercept.register({
+        responseError: function (error) {
+            // Handle an fetch error
+            console.log('responseError: ', error);
+            if (getToken()) {
+                localStorage.clear();
+                window.location = 'https://pixcollector.herokuapp.com/auth'; // redirect to AUTH
+            }
+            return Promise.reject(error);
         }
-        return [url, config];
-    },
+    }),
+    intercept: fetchIntercept.register({
+        request: function (url, config) {
+            if (getToken()) {
+                config.headers.Authorization = `x-csrf-token ${getToken()}`;
+            }
+            return [url, config];
+        },
 
-    requestError: function (error) {
-        // Called when an error occured during another 'request' interceptor call
-        // Handle an fetch error
-        console.log('requestError: ', error);
-        return Promise.reject(error);
-    },
+        requestError: function (error) {
+            // Called when an error occured during another 'request' interceptor call
+            // Handle an fetch error
+            console.log('requestError: ', error);
+            return Promise.reject(error);
+        },
 
-    response: function (response) {
-        if (response.code >= 400) throw new Error('Request error');
-        return response;
-    },
-
-    responseError: function (error) {
-        // Handle an fetch error
-        console.log('responseError: ', error);
-        if (getToken()) {
-            localStorage.clear();
-            window.location = 'https://pixcollector.herokuapp.com/auth'; // redirect to AUTH
+        response: function (response) {
+            if (response.code >= 400) throw new Error('Request error');
+            return response;
         }
-        return Promise.reject(error);
-    }
-});
+    })
+};
 
 // Unregister your interceptor
 // unregister();
