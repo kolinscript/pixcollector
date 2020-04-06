d2f30578e85252bcf0c1c9c62a79aba9567f7b45
nkoshkarov <koshkarovnik@gmail.com>
2020-04-06 13:38:34 +0300

    react axios test request and interceptors

--- client/src/interceptor.js
+++ client/src/interceptor.js
@@ -26,11 +26,12 @@ export default {
             return response;
         }, error => {
 
-            if (error.response.status === 401) {
-                console.log('** interceptor ** error response: ', error);
-                // localStorage.clear();
-//             // window.location = 'https://pixcollector.herokuapp.com/auth'; // redirect to AUTH
-            }
+            console.log('** interceptor ** error : ', error);
+            // if (error.response.status === 401) {
+            //     console.log('** interceptor ** error response: ', error);
+            //     // localStorage.clear();
+            //     // window.location = 'https://pixcollector.herokuapp.com/auth'; // redirect to AUTH
+            // }
 
             return Promise.reject(error);
         });
