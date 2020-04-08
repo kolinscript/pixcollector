cfa49a904cd5be6383b3f2b4fe5e91c5729116e4
nkoshkarov <koshkarovnik@gmail.com>
2020-04-08 20:12:09 +0300

    API photos.js test range downloads, CLIENT add logo, layout reworks etc

--- api/routes/v1/stock.js
+++ api/routes/v1/stock.js
@@ -5,7 +5,7 @@ router.get('/', secure.required, (req, res, next) => {
     // todo запрос новой информации из вк (альбом, etc) на основании текущего токена, обновление в базе
     if (req.session.user) {
         const user = req.session.user;
-        const safeUser = ({ _id, vkToken, ...rest }) => rest;
+        const safeUser = ({ _id, vkToken, token, ...rest }) => rest;
         res.status(200).json( { body: { user: safeUser(user) } });
     } else {
         res.status(200).json( { body: { error: 'found no user' } });
