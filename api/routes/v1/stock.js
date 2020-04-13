923da32bbe55b53399023969aa95e22c0059bbde
nkoshkarov <koshkarovnik@gmail.com>
2020-04-13 18:02:58 +0300

    gallery on work

--- api/routes/v1/stock.js
+++ api/routes/v1/stock.js
@@ -265,11 +265,6 @@ router.get('/', secure.required, (req, res, next) => {
             .catch(function (error) {
                 console.log(error);
             });
-
-
-        const user = req.session.user;
-        const safeUser = ({ _id, vkToken, token, ...rest }) => rest;
-        res.status(200).json( { body: { user: safeUser(user) } });
     } else {
         res.status(200).json( { body: { error: 'found no user' } });
     }
