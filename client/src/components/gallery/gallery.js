de08279505be62ebb356d71611e37b479627c5d8
nkoshkarov <koshkarovnik@gmail.com>
2020-04-13 18:28:53 +0300

    gallery on work

--- client/src/components/gallery/gallery.js
+++ client/src/components/gallery/gallery.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Gallery = (props) => (
     <div className="gallery">
         <div className="pixies">
-            {props.pixArray.slice(props.cutFrom - 1, props.cutTo + 1).map((value) => {
+            {(props.pixArray.length) && props.pixArray.slice(props.cutFrom - 1, props.cutTo + 1).map((value) => {
                 return (
                     <div className="pix">
                         <div className="pix-container">
