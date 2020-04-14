e03888bd81cbe468fdc9a50566f8f38c287aaebc
nkoshkarov <koshkarovnik@gmail.com>
2020-04-14 06:36:07 +0300

    gallery on work

--- client/src/components/stock.js
+++ client/src/components/stock.js
@@ -4,11 +4,11 @@ export const Stock = (props) => (
     <div className="stock">
         <div className="stock-card self">
             <div className="logo"></div>
-            <div className="subtitle">my gallery</div>
+            <div className="subtitle">my stock</div>
         </div>
         <div className="stock-card users">
             <div className="logo"></div>
-            <div className="subtitle">users galleries </div>
+            <div className="subtitle">users stocks</div>
         </div>
     </div>
 );
