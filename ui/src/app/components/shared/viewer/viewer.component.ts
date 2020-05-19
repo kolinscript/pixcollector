6574bc83082ecfb0aeeb132714e56f0a6c3ad864
nkoshkarov <koshkarovnik@gmail.com>
2020-05-19 05:52:01 +0300

    ui stock page updates

--- ui/src/app/components/shared/viewer/viewer.component.ts
+++ ui/src/app/components/shared/viewer/viewer.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-viewer',
