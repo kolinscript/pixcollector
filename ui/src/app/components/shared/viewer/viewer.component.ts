200444f2a9bc646b6352abcb329f6267ef9f7934
nkoshkarov <koshkarovnik@gmail.com>
2020-05-19 05:35:41 +0300

    ui stock page updates

--- ui/src/app/components/shared/viewer/viewer.component.ts
+++ ui/src/app/components/shared/viewer/viewer.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-viewer',
   templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.scss']
 })
-export class ViewerComponent implements OnInit {
+export class ViewerComponent implements OnInit, OnChanges {
   @Input() viewerPix;
   @Output() close = new EventEmitter<boolean>();
   @Output() slideTo = new EventEmitter<string>();
@@ -14,6 +14,10 @@ export class ViewerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.href = this.viewerPix.url;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
     this.href = this.viewerPix.url;
   }
 
