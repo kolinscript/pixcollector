d87ff94166d763afca58b3e8b093233dec573863
nkoshkarov <koshkarovnik@gmail.com>
2020-06-23 02:05:25 +0300

    test main functionality - download

--- api/middlewares/download.js
+++ api/middlewares/download.js
@@ -4,17 +4,18 @@ const ZipStream          = require('zip-stream');
 const download = {
     download: ((req, res, next) => {
         const pixArray = req.body.selected_pixies;
-        console.log(pixArray);
+        console.log('pixArray:___', pixArray);
         const zip = new ZipStream();
         zip.pipe(res);
         function addNextFile() {
             const elem = pixArray.shift();
+            console.log('elem:___', elem);
             const stream = request(elem.url);
             const name = elem.url.slice(elem.url.lastIndexOf('/'));
             zip.entry(stream, { name: name }, err => {
                 if (err)
                     throw err;
-                if (req.session.user.pixArray.length > 0)
+                if (pixArray.length > 0)
                     addNextFile();
                 else
                     zip.finalize();
