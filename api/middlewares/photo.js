8d4263175f8f59b54986d27c4a5ce9b551821d5c
nkoshkarov <koshkarovnik@gmail.com>
2020-07-21 21:41:57 +0300

    UI vk save & like testing

--- api/middlewares/photo.js
+++ api/middlewares/photo.js
@@ -8,37 +8,23 @@ const photo = {
     vkSave: (req, res, next) => {
         const owner_id = req.body.owner_id;
         const photo_id = req.body.photo_id;
-        const {headers: {authorization}} = req;
-        let token;
-        let tokenVkId;
-        if (authorization && authorization.split(' ')[0] === 'token') {
-            token = authorization.split(' ')[1];
-            tokenVkId = jwt.verify(token, 'collector_secret').vkId;
-        }
-        Users.findOne({vkId: tokenVkId}, (err, userDbFromToken) => {
-            if (err) {
-                res.status(200).json({body: {error: err}});
-            }
-            if (userDbFromToken) {
-                const link = `https://api.vk.com/` +
-                    `method/photos.copy` +
-                    `?access_token=${userDbFromToken.vkToken}` +
-                    `&owner_id=${owner_id}` +
-                    `&photo_id=${photo_id}` +
-                    `&v=5.120`;
-                axios.get(link)
-                    .then(function (response) {
-                        if (response.data) {
-                            res.status(200).json({body: {data: response.data}});
-                        }
-                    })
-                    .catch(function (error) {
-                        res.status(200).json({body: {error: {text: error, code: 5}}});
-                    });
-            } else {
-                res.status(200).json({body: {error: {text: 'found no userDbFromToken', code: 0}}});
-            }
-        });
+        const vkTokenIF = req.body.vkTokenIF;
+        const link = `https://api.vk.com/` +
+            `method/photos.copy` +
+            `?access_token=${vkTokenIF}` +
+            `&owner_id=${owner_id}` +
+            `&photo_id=${photo_id}` +
+            `&v=5.120`;
+        axios.get(link)
+            .then((response) => {
+                console.log('______response______', response);
+                if (response.data) {
+                    res.status(200).json({body: {data: response.data}});
+                }
+            })
+            .catch((error) => {
+                res.status(200).json({body: {error: {text: error, code: 5}}});
+            });
     },
 
     vkLike: (req, res, next) => {
--- ui/src/app/services/photo.service.ts
+++ ui/src/app/services/photo.service.ts
@@ -13,11 +13,10 @@ export class PhotoService {
   }
 
   public vkSave(owner_id: number, photo_id: number, vkTokenIF: string): Observable<any> {
-    const HEADERS = new HttpHeaders()
-      .set('Origin', 'pixcollector.herokuapp.com')
-      .set('Skip-Token', 'true');
-    const URL = `https://api.vk.com/method/photos.copy?access_token=${vkTokenIF}&owner_id=${owner_id}&photo_id=${photo_id}&v=5.120`;
-    return this.http.get(`${URL}`, {headers: HEADERS});
+    return this.http.post(
+      `/api/v1/vk-photo/save`,
+      {owner_id: owner_id, photo_id: photo_id, vkTokenIF: vkTokenIF}
+    );
   }
 
   public vkLike(owner_id: number, photo_id: number, vkTokenIF: string,): Observable<any> {
